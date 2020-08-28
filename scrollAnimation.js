b285f3efb2e3389a47f15a7f3d24e884b17cdf53
IAndresI <andrei200277@gmail.com>
2020-08-28 13:53:38 +0300

    scrollAnimation v1.1

--- scrollAnimation.js
+++ scrollAnimation.js
@@ -1,35 +1,47 @@
-function scrollAnimation({
+function startAnimation({
   elements,
   triggerClass,
-  triggerHeight,
+  triggerHeight = 100,
   infinity = false,
   trough = {
     number: 0,
     withFirst: false
   }
 }) {
-  function calculated(element) {
-    if ((pageYOffset + window.innerHeight - triggerHeight > element.getBoundingClientRect().y + pageYOffset) && (element.getBoundingClientRect().y + pageYOffset > pageYOffset - element.offsetHeight + triggerHeight)) {
+
+  function animationTrigger(element) {
+    if ((window.pageYOffset + window.innerHeight - triggerHeight > element.getBoundingClientRect().top + window.pageYOffset) && (element.getBoundingClientRect().top + window.pageYOffset > window.pageYOffset - element.offsetHeight + triggerHeight)) {
       element.classList.add(triggerClass);
-    } else {
-      if (infinity) {
-        element.classList.remove(triggerClass);
-      }
+      element.style.visibility = "visible";
+    } else if (infinity) {
+      element.classList.remove(triggerClass);
+      element.style.visibility = "hidden";
     }
   }
+
+  function trigger(element) {
+    animationTrigger(element);
+    window.addEventListener("scroll", () => {
+      animationTrigger(element);
+    });
+  }
+
   if (elements.length > 1) {
+    elements.forEach((element) => {
+      element.style.visibility = "hidden";
+    });
     if (trough) {
       let temp = trough.number;
       if (trough.withFirst) {
         elements.forEach((element, i) => {
           if (trough.withFirst) {
-            calculated(element);
+            trigger(element);
             trough.withFirst = false;
           }
 
           if (trough.withFirst == false) {
             if (i == temp + 1) {
-              calculated(element);
+              trigger(element);
               temp += trough.number + 1;
             }
           }
@@ -37,7 +49,7 @@ function scrollAnimation({
       } else {
         elements.forEach((element, i) => {
           if (i == temp) {
-            calculated(element);
+            trigger(element);
             temp += trough.number + 1;
           }
         });
@@ -46,7 +58,8 @@ function scrollAnimation({
   } else {
     let element;
     elements.length == 1 ? element = elements[0] : element = elements;
-    calculated(element);
+    element.style.visibility = "hidden";
+    trigger(element);
   }
 }
 
